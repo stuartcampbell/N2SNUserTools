c2a5b75373c56fcf2099119b8ae4931921abe961
Stuart Campbell <scampbell@bnl.gov>
2023-08-19 15:32:28 -0400

    Upgrade versioneer to 0.22

--- N2SNUserTools/__init__.py
+++ N2SNUserTools/__init__.py
@@ -1,7 +1,6 @@
 # flake8: noqa
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
 
 from .utils import (n2sn_list_group_users_as_table,
                     n2sn_list_user_search_as_table)
